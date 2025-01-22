69dff3a056f55b82850c2aebb85282b1706cac42
Lucky <116340727+sainilucky2808@users.noreply.github.com>
2025-01-22 22:13:05 +0530

    Add files via upload

---
+++ FirstDemo.java
@@ -0,0 +1,10 @@
+class FirstDemo
+	{
+	public static void main(String[] args) {
+	// single line comments
+	/* For
+	   Multiline Comments
+	*/
+	System.out.println("Hello World!");
+	}
+}
---
+++ MethodDemo.java
@@ -0,0 +1,12 @@
+class MethodDemo
+{
+	int a,b;
+	void add()
+	{
+		System.out.println("Sum : "+(a+b));
+	}
+	void sub()
+	{
+		System.out.println("Difference : "+(a-b));
+	}
+}
