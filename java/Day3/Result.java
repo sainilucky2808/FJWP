1fb41d4f0c382b4baa60d5bb6821c8e06abb5ba4
Lucky <116340727+sainilucky2808@users.noreply.github.com>
2025-01-23 15:46:35 +0530

    Add files via upload

---
+++ java/Day3/Result.java
@@ -0,0 +1,12 @@
+// Write a program to calculate and print the percentage of total marks where student has scored 430marks out of 500.
+
+class Result
+	{
+	public static void main(String args[])
+	{
+	int TM = 500;
+	int OM = 430;
+	double Percentage = (OM*100)/TM;
+	System.out.println("Percentage: "+Percentage);
+	}
+}
