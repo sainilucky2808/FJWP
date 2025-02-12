26ba99ece34800c0ce56f5b1be346f6ecae9eb3a
Lucky <116340727+sainilucky2808@users.noreply.github.com>
2025-02-12 13:35:25 +0530

    Add files via upload

---
+++ java/Lab1/Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle{
+	double width;
+	double height;
+	enum Color {
+	RED, GREEN, BLUE
+	}
+	Color boxColor;
+	public static void main(String[] args)
+	{
+	System.out.println("Selected Color: "+ Color.BLUE);
+	}
+}
---
+++ java/Lab1/Student.java
@@ -0,0 +1,10 @@
+class Student{
+	
+	private String studentName = "Lalit";
+	private String collegeName = "DPGITM";
+	private int studentID = 2271;
+
+	public static void main(String[] args){
+	System.out.println("Successful");
+	}
+}
