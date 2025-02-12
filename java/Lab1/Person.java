5e8295262c555dcbc946edaa7d0baf41520e7298
Lucky <116340727+sainilucky2808@users.noreply.github.com>
2025-02-12 13:36:26 +0530

    Add files via upload

---
+++ java/Lab1/Person.java
@@ -0,0 +1,11 @@
+package com.anudip.learning;
+
+class Person{
+	String name = "Lalit";
+	int age = 23;
+	int salary = 25500;
+	public static void main(String[] args)
+	{
+	System.out.println("Test Successful");
+	}
+}
