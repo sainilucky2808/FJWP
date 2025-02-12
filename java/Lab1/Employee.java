e92399fac76bb2f28f208786c1e587c5b6d92ddf
Lucky <116340727+sainilucky2808@users.noreply.github.com>
2025-02-12 13:31:33 +0530

    Create Employee.java

---
+++ java/Lab1/Employee.java
@@ -0,0 +1,13 @@
+class Employee{
+	
+	protected int id;
+	protected int age;
+	protected String name;
+	protected boolean isPermanent;
+	public static void main(String[] args)
+	{
+	Employee employee = new Employee();
+	employee.age = (int) 35.5;
+	System.out.println("Successfully started");
+	}
+}
